fc2175c20f69f42f4b2d86095e3b77ec841b3367
mattlandry <mattlandry2@gmail.com>
2017-11-26 09:51:57 -0600

    Updated testing harness

--- __tests__/test-example-work.js
+++ __tests__/test-example-work.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import ExampleWork from '../js/example-work';
+import ExampleWork, { ExampleWorkBubble } from '../js/example-work';
 configure ({ adapter: new Adapter()});
 
 const myWork = [
@@ -31,6 +31,24 @@ describe("ExampleWork component", () => {
   let component = shallow(<ExampleWork work={myWork}/>);
 
     it("Should be a 'section' element", () => {
-      console.log(component.debug());
+      expect(component.type()).toEqual('section');
   });
+
+  it("Should contain as many children as there are work examples", () => {
+    expect(component.find("ExampleWorkBubble").length).toEqual(myWork.length);
+  });
+
+});
+
+describe("ExampleWorkBubble component", () => {
+  let component = shallow(<ExampleWorkBubble example={myWork[1]}/>);
+  let images = component.find("img");
+
+  it("Should contain a single 'img' element", () => {
+    expect(images.length).toEqual(1);
+  });
+
+  it("Should have the image src set correctly", () => {
+    expect(images.getElement().props.src).toEqual(myWork[1].image.src);
+  })
 });
