3002a52dcc921c0f0eb6dfa678bba73af277879e
mattlandry <mattlandry2@gmail.com>
2017-11-26 20:15:51 -0600

    lots of reductions

--- __tests__/test-example-work.js
+++ __tests__/test-example-work.js
@@ -2,32 +2,13 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import ExampleWork, { ExampleWorkBubble } from '../js/example-work';
+import App from '../js/homepage';
 configure ({ adapter: new Adapter()});
 
-const myWork = [
-  {
-    'title': "Work Example 1",
-    'image': {
-      'desc': "example screenshot of a project involving code",
-      'src': "images/example1.png",
-      'comment': ""
-    }
-  },
-  {
-    'title': "Work Example 3",
-    'image': {
-      'desc': "serverless portfolio",
-      'src': "images/example3.png",
-      'comment': `Bengal cat” by roberto shabs is licensed under CC BY 2.0
-                  https://www.flickr.com/photos/37287295@N00/2540855181`
-    }
-  }
-];
 
 
 
-describe("ExampleWork component", () => {
+describe("EZ Test", () => {
   let component = shallow(<ExampleWork work={myWork}/>);
 
     it("Should be a 'section' element", () => {
